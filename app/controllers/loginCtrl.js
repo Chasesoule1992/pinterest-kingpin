6a94b8f47e611a1e75a43bf69c793ab0d155d49d
Ben Bledsoe <bledsoedesigns@gmail.com>
2015-12-08 18:19:45 -0600

    merged bry and added filtering for all things

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -14,6 +14,7 @@ app.controller("loginCtrl", ["$q", "$http", "$scope", "$firebaseArray", "general
 
 		var ref = new Firebase("https://kingpinteam.firebaseio.com/users");
 
+
 		//register user
 		$scope.registerUser  = function(){
 			if( $scope.loginEmail !== "" && $scope.loginPassword !== "" && $scope.loginEmail !== undefined && $scope.loginPassword !== undefined){
@@ -61,6 +62,8 @@ app.controller("loginCtrl", ["$q", "$http", "$scope", "$firebaseArray", "general
 	    console.log("Authenticated successfully with payload:", authData);
 	    $("#loginSplash").css({"display":"none"});
 	    $("#mainPage").fadeIn("slow");
+	    generalVariables.setUid(authData.uid)
+
 
 	    var pinRef = new Firebase("https://kingpinteam.firebaseio.com/pins");
 
@@ -69,68 +72,12 @@ app.controller("loginCtrl", ["$q", "$http", "$scope", "$firebaseArray", "general
 	    pinArray
 	    .$loaded()
 	    .then(function(data){
-	    	console.log("hey", data);
-	    	loadedPins = data;
 
-			    //go to current user uid/tags
-			    	//convert string of tags into array separated by commas
-			    	//for each item in array
-			    	//go into pins object
-			    		//if pinID.tags === current item in array
-			    		//output something to screen
-			    console.log("authData ", authData.uid);
+	    	$scope.matchedArray = pinArray;
 
+	    	console.log(generalVariables.getUid());
 
-			    // BEN GOT THE UID HERE AND PASSED IT TO THE FACTORY FOR LATER USE
-
-			    generalVariables.setUid(authData.uid);
-			    
-			    console.log("user id ", generalVariables.getUid());
-
-			//go to current user uid/tags
-			   var userRef = new Firebase("https://kingpinteam.firebaseio.com/users/"+authData.uid);
-
-			   //this is how you get data from firebase
-			   userRef.on("value", function(snapshot) {
-				  console.log(snapshot.val().tags);
-
-				  //convert string of tags into array separated by commas
-			    	var tags = snapshot.val().tags.split(", ");
-
-			    	console.log("tags ", tags);
-
-			    	//for each item in tags array (tags of current user)
-			    	for(var i = 0; i < tags.length; i++){
-
-			    		console.log("currents", loadedPins);
-
-
-
-			    		for(var x = 0; x < loadedPins.length; x++){
-			    			// console.log("hey", loadedPins[x].tags);
-			    			// console.log("tags[i]", tags[i]);
-
-			    			if(loadedPins[x].tags === tags[i]){
-			    				console.log("loadedPinsx", loadedPins[x]);
-			    				console.log("match sah");
-			    				$scope.matchedArray.push(loadedPins[x]);
-
-			    				//$scope.$apply is used when using anything not in angular fire (basically reupdate page)
-			    				$scope.$apply();
-			    			}
-			    		}
-
-			    		console.log("matchedArray", $scope.matchedArray);
-
-			    	}
-
-
-				}, function (errorObject) {
-				  console.log("The read failed: " + errorObject.code);
-				});
-
-
-			  });
+	    	});
 
 			}
 	    });
